1802a1a31ff62606e7609ea321d8dcb33c83dede
Pavel Khegay <pashulia.pk@gmail.com>
2023-04-03 20:51:50 +0600

    add deploy and contract funtions

--- hw_51/src/contracts/example.sol
+++ hw_51/src/contracts/example.sol
@@ -1,24 +1,15 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
 
 contract Example {
     uint256 number;
-    string str;
     uint256[] arr;
 
     function setNumber(uint256 _number) public {
         number = _number;
     }
-
-    function getStr() public view returns(string memory){
-        return str;
-    }
-
-    function setStr(string memory _str) public {
-        str = _str;
-    }
-
+    
     function getNumber() public view returns(uint256){
         return number;
     }
